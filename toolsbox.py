3f48468bda210d4977c4eb88c6aff420b3656591
xueqi_147 <421790146@qq.com>
2017-06-08 15:19:45 +0800

    添加了异常捕获

--- toolsbox.py
+++ toolsbox.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from toolsbox_ui import Ui_toolsbox
 import ipaddress
 import json
+import logging
 
 class MyFirstGuiProgram(Ui_toolsbox):
     def __init__(self, dialog):
@@ -16,21 +17,30 @@ class MyFirstGuiProgram(Ui_toolsbox):
         self.jsonformat_run.clicked.connect(self.jsonformat_proc)
     
     def ipconvert_num2str_proc(self):
-        numip = self.ipconvert_num2str_input.text()
-        strip = ipaddress.ip_address(int(numip))
-        print(strip)
-        self.ipconvert_num2str_output.setText(str(strip))
+        try:
+            numip = self.ipconvert_num2str_input.text()
+            strip = ipaddress.ip_address(int(numip))
+            print(strip)
+            self.ipconvert_num2str_output.setText(str(strip))
+        except Exception as e:
+            logging.exception(e)
         
     def ipconvert_str2num_proc(self):
-        strip = self.ipconvert_str2num_input.text()
-        numip = int(ipaddress.ip_address(strip))
-        self.ipconvert_str2num_output.setText(str(numip))
+        try:
+            strip = self.ipconvert_str2num_input.text()
+            numip = int(ipaddress.ip_address(strip))
+            self.ipconvert_str2num_output.setText(str(numip))
+        except Exception as e:
+            logging.exception(e)
         
     def jsonformat_proc(self):
-        text = self.jsonformat_text.toPlainText()
-        json_obj = json.loads(text)
-        text = json.dumps(json_obj, indent=4)
-        self.jsonformat_text.setPlainText(text)
+        try:
+            text = self.jsonformat_text.toPlainText()
+            json_obj = json.loads(text)
+            text = json.dumps(json_obj, indent=4)
+            self.jsonformat_text.setPlainText(text)
+        except Exception as e:
+            logging.exception(e)
         
         
         
